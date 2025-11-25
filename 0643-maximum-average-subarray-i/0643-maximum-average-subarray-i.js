b1008f1fcec3bcfca7783a460f9d90465211f257
Niranjan Das <niranjandas.dev@gmail.com>
2025-11-25 10:09:44 +0530

    Time: 2 ms (82.23%), Space: 67.9 MB (45.1%) - LeetHub

---
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var findMaxAverage = function(nums, k) {
+    let windowSum = 0;
+
+    // Sum of the first window
+    for (let i = 0; i < k; i++) {
+        windowSum += nums[i];
+    }
+
+    let maxSum = windowSum;
+
+    // Slide the window
+    for (let i = k; i < nums.length; i++) {
+        windowSum += nums[i] - nums[i - k];
+
+        if (windowSum > maxSum) {
+            maxSum = windowSum;
+        }
+    }
+
+    return maxSum / k;
+};
