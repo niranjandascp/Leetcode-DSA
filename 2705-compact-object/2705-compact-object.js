034b13d8516693754f20b2a6af5ef2bf4e5679df
Niranjan Das <niranjandas.dev@gmail.com>
2025-12-03 10:56:23 +0530

    Time: 79 ms (47.13%), Space: 65.3 MB (70.3%) - LeetHub

---
+++ 2705-compact-object/2705-compact-object.js
@@ -0,0 +1,25 @@
+/**
+ * @param {Object|Array} obj
+ * @return {Object|Array}
+ */
+var compactObject = function(obj) {
+     if (obj === null || typeof obj !== "object") {
+        return obj ? obj : undefined;
+    }
+
+    if (Array.isArray(obj)) {
+        let result = [];
+        for (let item of obj) {
+            let compacted = compactObject(item);
+            if (Boolean(compacted)) result.push(compacted);
+        }
+        return result;
+    }
+
+    let result = {};
+    for (let key in obj) {
+        let compacted = compactObject(obj[key]);
+        if (Boolean(compacted)) result[key] = compacted;
+    }
+    return result;
+};
