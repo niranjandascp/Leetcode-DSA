d1433c7e6ce4aaeabe3232e9b7c3273fc5cb57e5
Niranjan Das <niranjandas.dev@gmail.com>
2025-12-07 21:13:15 +0530

    Time: 2 ms (66.56%), Space: 60.8 MB (60.99%) - LeetHub

---
+++ 1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var longestSubarray = function(nums) {
+    let left = 0, zeros = 0, maxLen = 0;
+
+    for (let right = 0; right < nums.length; right++) {
+        if (nums[right] === 0) zeros++;
+
+        while (zeros > 1) {
+            if (nums[left] === 0) zeros--;
+            left++;
+        }
+
+        maxLen = Math.max(maxLen, right - left);
+    }
+
+    return maxLen;
+};
