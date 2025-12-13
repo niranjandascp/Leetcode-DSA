de8547481ee6658470fbff04cdfec39a8344f665
Niranjan Das <niranjandas.dev@gmail.com>
2025-12-13 16:32:35 +0530

    Time: 5 ms (54.79%), Space: 56.8 MB (65.44%) - LeetHub

---
+++ 0735-asteroid-collision/0735-asteroid-collision.js
@@ -0,0 +1,33 @@
+/**
+ * @param {number[]} asteroids
+ * @return {number[]}
+ */
+var asteroidCollision = function(asteroids) {
+    const stack = [];
+
+    for (let ast of asteroids) {
+        let alive = true;
+
+        while (
+            alive &&
+            stack.length &&
+            stack[stack.length - 1] > 0 &&
+            ast < 0
+        ) {
+            const top = stack[stack.length - 1];
+
+            if (Math.abs(top) < Math.abs(ast)) {
+                stack.pop(); 
+            } else if (Math.abs(top) > Math.abs(ast)) {
+                alive = false;
+            } else {
+                stack.pop();
+                alive = false;
+            }
+        }
+
+        if (alive) stack.push(ast);
+    }
+
+    return stack;
+};
